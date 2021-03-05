f64f6e42909b6b4cc559368625d88ce01a3b8101
Satya Raju <satya.kusampudi@gmail.com>
2021-03-05 14:55:53 +0530

    second commit for home and index html changes

--- django_tutorial/first_app/urls.py
+++ django_tutorial/first_app/urls.py
@@ -3,8 +3,8 @@ from .views import home
 from .views import about, news, get_dept_details, get_area_details, get_employee_details
 
 urlpatterns = [
-    path('home/', home),
-    path('about/', about),
+    path('home/', home ,name= 'home'),
+    path('about/', about, name='about'),
     path('news/', news),
     path("departments/<int:dept_id>/",get_dept_details),
     path("areas/<int:area_id>/", get_area_details),
